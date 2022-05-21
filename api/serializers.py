2c16d4890f3c0dbd206cd7791b6091ab2ea8e6e4
Lucas Guilherme <lucasguilherme_@hotmail.com>
2022-05-20 23:22:38 -0300

    Adicionado arquivo .gitignore

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from usuarios import models
-
-class UsuariosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Usuario
-        fields = '__all__'
--- api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from api import serializers
-from usuarios import models
-
-class UsuariosViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.UsuariosSerializer
-    queryset = models.Usuario.objects.all()
